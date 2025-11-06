aba3ba0f3ce674a2127d7997947d4b7c23b5458a
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2025-11-06 15:36:30 +0000

    Time: 0 ms (100%), Space: 18.7 MB (32.76%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,13 +1,18 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        showed = {} # key: num, val: idx
-        
-        # cuurent num_c + num_p in {showed nums} = target
-        # add num_c in {showed nums}
-        for idx, num in enumerate(numbers):
-            if (target-num) in showed:
-                return [showed[target-num]+1,idx+1]
-            
-            showed[num] = idx
+        # make use of SORTED: two pointers
+        left = 0
+        right = len(numbers)-1
 
+        while left<right:
+            s = numbers[left] + numbers[right]
+            if s == target:
+                break
+            if s < target:
+                left += 1
+            else:
+                right -= 1
+
+        return [left+1,right+1]
         
+        # Time: O(n), Space: O(1)
