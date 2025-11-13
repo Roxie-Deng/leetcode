63ca212e84e79f33af882157ae36ae13b5c49f8b
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2025-11-13 16:17:32 +0000

    Time: 0 ms (100%), Space: 19.2 MB (20.28%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,43 +1,23 @@
 class Solution:
+    def lowerbound(self,nums, target):
+        # 返回 最小的 满足nums[i]>=target的 下标
+        left=0
+        right=len(nums)-1 # [left,right]
+        while left<= right:
+            mid = (left+right)//2
+            if nums[mid]>=target:
+                right = mid - 1
+            else:
+                left = mid +1
+        return left
+
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         # O(log n): binary search [left,right]
         if not nums:
             return [-1,-1]
 
-        # search scope
-        left = 0
-        right = len(nums) - 1
-        # target index scope
-        start = len(nums)
-        end = -1
-
-        # find min index
-        while left<=right:
-            mid = left + (right-left)//2
-            if nums[mid] == target:
-                start = min(start, mid) # record current index
-                right = mid - 1 # keep searching in the left half
-            elif nums[mid] <target:
-                left = mid+1
-            else:
-                right = mid-1
-
-        # search scope
-        left = 0
-        right = len(nums) - 1
-        
-        # find max index
-        while left<=right:
-            mid = left + (right-left)//2
-            if nums[mid] == target:
-                end = max(end,mid) # record current index
-                left = mid + 1 # keep searching in the right half
-            elif nums[mid] <target:
-                left = mid+1
-            else:
-                right = mid-1
-
-        if start==len(nums):
-            start = -1
-        
+        start = self.lowerbound(nums,target)
+        if start == len(nums) or nums[start] != target:
+            return [-1,-1]
+        end = self.lowerbound(nums,target+1)-1
         return [start,end]
