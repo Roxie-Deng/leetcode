de07897e4880f343a9c8791936a929332f1b7ad3
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2025-11-27 18:50:10 +0000

    Time: 1 ms (13.69%), Space: 17.6 MB (87.59%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        # 记录每一层最右的节点，先右后左
+        ans = []
+        def dfs(node,depth): # 当前节点和所在层
+            if node is None:
+                return
+            if depth == len(ans):
+                ans.append(node.val)
+            dfs(node.right,depth+1)
+            dfs(node.left,depth+1)
+        dfs(root,0)
+        return ans
+    # O(n);O(depth)
