7347138eae1e1f5d2dee80aa26b7ec89447ab549
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-06-22 17:50:53 +0800

    Time: 0 ms (100%), Space: 19.4 MB (41.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -16,6 +16,8 @@ class Solution:
 
         # 使用了记忆化递归（@cache）后，状态数为 m*n，每个状态只计算一次，所以时间复杂度为 O(m×n)。空间上，缓存本身占 O(m×n)，递归栈深度为 O(m+n)，因此总空间复杂度为 O(m×n)。
         '''
+
+        '''
         # 1:1翻译为dp迭代
         # dp[r][c] = dp[r-1][c]+dp[r][c-1] 从(0,0)到达(r,c)经历的unique paths
         # 新建m行n列表格
@@ -32,3 +34,15 @@ class Solution:
         
         return dp[m-1][n-1]
         # O(mn);O(mn)
+        '''
+
+        # 继续优化，我们其实没有必要记录每一行，可以不断地覆盖上一行即可,二维数组变一维
+        dp = [1]*n # 第一行均为1
+
+        for r in range(1,m):
+            for c in range(1,n):
+                dp[c] = dp[c-1] + dp[c] # (1,1) = (0,1)+(1,0)
+        
+        return dp[-1]
+        # O(mn);O(n)
+            
