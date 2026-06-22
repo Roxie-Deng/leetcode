8cb84a1f0e8b2d286306b4a4fc9eed86f5e23405
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-06-22 17:01:04 +0800

    Time: 19 ms (76.09%), Space: 25.7 MB (45.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -19,14 +19,16 @@ class Solution:
         # answer[1] = prefix[1] * rev_prefix[2]
 
         n = len(nums)
-        prefix = [1]*(n+1)
-        rev_prefix =[1]*(n+1)
-        answer = [1]*n
-        for i in range(n):
-            prefix[i+1] = prefix[i]*nums[i]
-            rev_prefix[i+1] = rev_prefix[i]*nums[n-1-i] 
-        for i in range(n):
-            answer[i] =  prefix[i]*rev_prefix[n-1-i]
+        ans = [1]*n
         
-        return answer
-        # O(n); O(n)
+        # 只用一个答案数组来节省空间，先从左到右
+        for i in range(1,n):
+            ans[i] = ans[i-1] * nums[i-1] # nums[i]的左侧乘积
+        # 再从右到左
+        rev = 1 # nums[i]的右侧乘积
+        for i in range(n-1,-1,-1):
+            ans[i] = ans[i] * rev # 左侧乘积*右侧乘积
+            rev *= nums[i] # 更新右侧乘积
+        return ans
+        
+        # O(n); 除ans外空间O(1)
