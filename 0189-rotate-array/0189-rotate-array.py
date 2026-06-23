32fe8bb8d36333c78e01337be1971c3b9f801147
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-06-23 21:01:35 +0800

    Time: 7 ms (55.16%), Space: 26.4 MB (70.29%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,7 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = k%len(nums)
-        nums[:] = nums[-k:] + nums[:-k]
+        # k = k%len(nums)
+        # nums[:] = nums[-k:] + nums[:-k]
         # 没修改引用但实际上在底层会创建临时代码
         # O(n);O(n)
+
+        # 反转整个数组 -> 反转[0,k-1] -> 反转[k,n-1]
+        def reverse(left,right):
+            while left<right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+
+        n = len(nums)
+        k = k%n
+        reverse(0,n-1)
+        reverse(0,k-1)
+        reverse(k,n-1)
+        # O(n); O(1)
