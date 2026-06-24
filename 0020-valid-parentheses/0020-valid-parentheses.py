73478a0719fce264234a6697ec29c6ebda37f780
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-06-24 18:08:32 +0800

    Time: 3 ms (33.7%), Space: 19.3 MB (27.16%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,14 +1,26 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        pair = {')':'(', ']':'[', '}':'{'}
-        checking = [] # stack
+        # 配对处理
+        def isPair(c1:str,c2:str) -> bool:
+            pair = c1+c2
+            if pair == "()" or pair == "[]" or pair == "{}":
+                return True
+            return False
 
-        for c in s:
-            if c in pair.values(): # opening
-                checking.append(c) # 入栈
-            else: # closing, 检验checking顶上是否是匹配的opening
-                if (not checking) or (checking.pop() != pair[c]):
+        if len(s) == 0 or len(s) % 2 != 0:
+            return False
+
+        left = {'(','[','{'}
+        right = {')',']','}'}
+        # 利用stack后进先出的特性，遇到left就入栈，遇到right就left和right一起出栈,最后检查栈是否为空
+        ans = []
+
+        for i in range(len(s)):
+            if s[i] in left:
+                ans.append(s[i])
+            if s[i] in right:
+                if not ans or not isPair(ans[-1],s[i]):
                     return False
-        
-        return not checking # 检验opening是否全部出栈
+                ans.pop()
+        return ans == []
         # O(n); O(n)
