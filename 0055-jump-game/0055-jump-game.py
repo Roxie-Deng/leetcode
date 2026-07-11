b74dbf871ca1ae0039ebaf777c6458df7953f0c0
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-07-11 19:19:16 +0800

    Time: 20 ms (54.43%), Space: 20.5 MB (17.05%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,12 +1,22 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # 全局的能跳到的最远位置，初始值为0
-        mr = 0
+        # new_idx = i+[0,nums[i]]
+        # target_idx = n-1
+        # brutal: all possible paths, until true -> inefficient
+        # 我们只需要知道能不能走出去,final_idx>=target_idx
+        # 所以找到能走的最远位置就可以了
+        # 从左往右遍历，维护一个变量furthest表示到目前为止全局能到的最远索引，如果当前索引i>furthest，说明不可能到达i更别提n-1；否则，更新furthest = max(furthest,i+nums[i])
+
+        n = len(nums)
+        furthest = 0
         
-        # loop over (i,nums[i])
-        for i,num in enumerate(nums):
-            if mr<i: # 永远无法到达最右端
+        for i in range(n):
+            if furthest>=n-1:
+                return True
+
+            if furthest>=i:
+                furthest = max(furthest,i+nums[i])
+            else:
                 return False
-            # update mr
-            mr = max(mr,i+num) # 比较当前下标能跳的最远位置和全局能跳的最远位置
-        return True
+        # O(n);O(1)
+            
