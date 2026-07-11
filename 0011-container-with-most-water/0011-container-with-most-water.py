c64318e4d97458227e6c49b89649cc1fc8ba707d
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-07-11 16:32:12 +0800

    Time: 47 ms (92.13%), Space: 29.6 MB (67.47%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,19 +1,19 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # max_area=min(height[l],height[r])*(r-l)
-        # r-l 必然会逐渐缩小
-        # 高尽量取更大
-        l,r = 0, len(height)-1
-        cur_max = 0
-
+        # width*height
+        # width = right-left
+        # height = min(height[left],height[right])
+        # left,right move towards from two edges
+        # width decreases; try to find higher height
+        n = len(height)
+        l,r = 0, n-1
+        cur_max=0
         while l<r:
-            width = r-l
             if height[l]<height[r]:
-                # 现在以左为高，算完面积收缩左边
-                cur_max = max(cur_max,height[l]*width)
+                cur_max = max(cur_max,(r-l)*height[l])
                 l += 1
             else:
-                cur_max = max(cur_max,height[r]*width)
+                cur_max = max(cur_max,(r-l)*height[r])
                 r -= 1
         return cur_max
         # O(n);O(1)
