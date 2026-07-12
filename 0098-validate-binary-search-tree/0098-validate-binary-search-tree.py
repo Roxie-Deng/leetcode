47e0e26601c5d2a09439c9fcd1f518652bf57b34
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-07-12 18:15:00 +0800

    Time: 3 ms (33.87%), Space: 20.9 MB (73.71%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -5,24 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    pre = -inf #上一个节点值
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # 左树所有节点 < root <右树所有节点
-        # 中序遍历：inorder(left) root inorder(right)
-        # BST的中序遍历：must be an ascending seq, 当前节点值一定比上一个节点值要大
+        # left<root<right
+        # 天然成为一个inorder
+        # 并且不是只比较一层，左子树所有节点都得比root小，右子树都比root大
+        # -inf<左子树<root<右子树<inf
 
-        if root is None:
-            return True
-        # visit left tree
-        if not self.isValidBST(root.left):
-            return False
-        # visit root
-        if root.val <= self.pre:
-            return False
-
-        # update state var
-        self.pre = root.val
-        # visit right tree
-        return self.isValidBST(root.right)
+        # 左:-inf<node.left<node.val,右: 上一层node.val<node.right<inf
+        # 设计三个变量
+        def dfs(low:int,node:Optional[TreeNode],high:int) -> bool:
+            if node is None:
+                return True
+            if not(low<node.val<high):
+                return False
             
-        
+            return dfs(low,node.left,node.val) and dfs(node.val,node.right,high)
+
+        return dfs(float('-inf'), root, float('inf'))
+        # O(n);O(n)
+        # dfs(root.left,root.right) 这种双参数一般用于对称/相同树
