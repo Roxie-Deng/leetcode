2e8e8c5d9da283d2cd0a6b2feaed6f62f7dd9fd3
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-07-12 17:42:48 +0800

    Time: 0 ms (100%), Space: 19.4 MB (53.4%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,12 +6,22 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        # p.left == q.right, p.right == q.left
-        def dfs(p,q):
-            if p is None or q is None:
-                return p is q
-            if p.val == q.val:
-                return dfs(p.left,q.right) and dfs(p.right, q.left)
-            return False
+        # left = right, left.left = right.right,left.right = right.left
+        # 左树: left-root-right, 右树:right-root,left
+        l_tree = root.left
+        r_tree = root.right
         
-        return dfs(root.left,root.right)
+        def dfs(node1, node2):
+            if not node1 or not node2:
+                return node1 == node2
+                # 节点 or 节点，if False, 跳过
+                # 节点 or None, if True, return False
+                # None or None, if True, return True
+            if node1.val != node2.val: # 两个节点，值不相等
+                return False
+            
+            # 两个节点，值相等，这才进入下一层
+            return dfs(node1.left,node2.right) and dfs(node1.right,node2.left)
+
+        return dfs(l_tree,r_tree)
+        # O(n);O(n),平均log(n)
