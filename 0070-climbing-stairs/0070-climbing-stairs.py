529ee2c4d6031f30f2ee4f207d75b3874351f63e
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-07-12 16:54:18 +0800

    Time: 0 ms (100%), Space: 19.3 MB (52.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # 计算不同路径->dp
-        # f(i) = f(i-1)+f(i-2) 变形斐波那契额数列；状态转移方程；递归->dp
-        if n <= 2:
-            return n
-        a = 1
-        b = 2
-        for _ in range(3,n+1): 
-            a,b = b,a+b # 滚动更新
-        return b
+        # dp[n] = dp[n-1]+dp[n-2]
+        # dp[1] = 1, dp[0] = 1
+        dp = [0]*(n+1)
+
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2,n+1):
+            dp[i] = dp[i-1]+dp[i-2]
+
+        return dp[n]
