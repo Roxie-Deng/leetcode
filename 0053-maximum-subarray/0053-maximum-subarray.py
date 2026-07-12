ee010bdb06991fe7dfd0b212826ed7be2e8fa3da
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-07-12 19:02:21 +0800

    Time: 41 ms (38.27%), Space: 31.4 MB (73.73%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int: 
+        '''
         # prefix f(i)=到i为止的和
         # f(i)最大-f(j)最小，if f(i)正f(j)负
         # f(i) - 0, if f(i)负f(j)负
@@ -16,4 +17,12 @@ class Solution:
             base = min(prefix[i],base)
         return ans
         # O(n);O(n)
-        
+        '''
+        # 继续优化: 不需要两次for和前缀和，可以实时DP，f(i)=到i时的最小子数组和，维护一个变量cur_max，状态转移：如果num加上cur_max反而变小，丢掉cur_max从num重新开始累加。再维护一个全局变量ans
+        cur_max = 0
+        ans = float('-inf')
+
+        for n in nums:
+            cur_max = max(cur_max+n, n)
+            ans = max(ans,cur_max)
+        return ans
