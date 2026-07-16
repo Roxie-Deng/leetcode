ea66bc8a1a158dcdd9a4e9c47eec812361fad727
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-07-16 17:22:52 +0800

    Time: 1281 ms (28.29%), Space: 19.3 MB (97.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,22 +1,18 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # 不同路径；求最值-> dp
-        # f(i) = f(i-1)+1, if nums[i]>nums[j] for j in range(i)
-        # f(i) = f(i-1), if nums[i]<=nums[j] for j in range(i)
-        # f(i) = 1, i=0
+        # brutal: 对于每一个num，遍历它剩下数组得到len_LIS，再比较
+        # 求最值：当前直接决定 vs 需要尝试多种可能性，本题显然是后者
+        # dp[i] 代表以nums[i]结尾的最长序列
+        # dp[i] = max(dp[j])+1 for all j<i and nums[j]<nums[i]
+        # dp[0] = 1
+        # return max(dp)
 
-        @cache
-        def dfs(i):
-            # 递推： dfs(i) = max(dfs(j))+1,如果没有任何j满足则就是1
-            res = 0
+        n = len(nums)
+        dp = [1]*n
+
+        for i in range(1,n):
             for j in range(i):
-                if nums[j]<nums[i]:
-                    res = max(res,dfs(j))
-            return res+1
-
-        return max(dfs(i) for i in range(len(nums))) # 所有位置的最大值
-        
-        # 状态数：n；非递归操作量：n
-        # O(n^2)
-        # 递归深度:n; 缓存大小:n
-        # O(n)
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i],dp[j]+1)
+        return max(dp)
+        # O(n^2);O(n)
