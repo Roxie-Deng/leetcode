860d7c7774d3981771b86f1944705af0cdc77c2a
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-07-16 20:50:09 +0800

    Time: 11 ms (83.14%), Space: 22 MB (65.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # 去重 -> hash
-        # Counter or 字符串排序（考虑到key的创建选后者）
-        
-        group = defaultdict(list) # 只要key不存在自动创建空列表
+        # 本质上是去重
+        # hash str: Counter or 字符串排序（后者明显更简单）
+        ans = defaultdict(list) # sorted_s:s1,s2 # O(n*k) space
 
-        for s in strs:
-            sorted_s = ''.join(sorted(s)) # eat -> aet
-            group[sorted_s].append(s) # {'eat':['eat','ate','tea']}
-        
-        ans = list(group.values())
-
+        for s in strs: # O(n) time
+            sorted_s_list = sorted(s) # 不改变原有的s # O(klogk) time; O(k) space
+            sorted_s = "".join(sorted_s_list) # O(k) time; O(k) space
+            ans[sorted_s].append(s)
+        ans = list(ans.values())
         return ans
 
-        # O(n*k*logk); O(n*k)
-        # 实际处理的总字符数 O(n*k)
-            
+        # O(n*klogk);O(n*k)
