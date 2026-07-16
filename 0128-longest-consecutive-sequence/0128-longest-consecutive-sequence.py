303b56e15d6735921cd0c86dfb2bdb44ba0cb91f
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-07-16 17:56:29 +0800

    Time: 46 ms (74.22%), Space: 36 MB (90.88%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,18 +1,23 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # O(n): 不能排序；不能嵌套
-        # for遍历O(n) -> 操作每个元素的复杂度必须为O(1) -> Hash
-        unique = set(nums)
+        if len(nums) <= 1:
+            return len(nums)
+        # O(n) time: NO sorting
+        # 一边遍历一边查找, 操作还得是O(1) -> hash -> set的成员检查
+
+        # 只需要unique nums
+        set_nums = set(nums) # O(n);O(n)
         longest = 0
 
-        for x in unique:
-            # 只有当 x 是序列的起点时开始计数
-            if x-1 not in unique:
-                cur_num = x
+        for x in set_nums:
+            if x-1 not in set_nums: # 只有当x是起点时开始计数 # 因为有这层过滤，即便是for套while，每个x也只被访问一次
                 cur_len = 1
-                while cur_num + 1 in unique:
-                    cur_num += 1
+                cur_num = x+1
+                while cur_num in set_nums:
                     cur_len += 1
+                    cur_num += 1
                 longest = max(longest,cur_len)
         return longest
-        # O(n):O(n)
+        # O(n);O(n)
+
+        
