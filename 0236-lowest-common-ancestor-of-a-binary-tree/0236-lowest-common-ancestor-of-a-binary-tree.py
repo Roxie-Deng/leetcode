ff61f813b7ffd3af7717cd2c87ba4df8b9a8953c
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-07-20 23:45:35 +0800

    Time: 145 ms (16.58%), Space: 49.1 MB (68.19%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,15 +7,22 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # 当前节点：1.为空 2.为p 3.为q -> 返回当前节点
-        if root == None or root == p or root == q:
-            return root
-        left = self.lowestCommonAncestor(root.left, p, q) # 左子树搜索结果: None 或某节点
-        right = self.lowestCommonAncestor(root.right, p, q)
-        if left and right: # 左右都不为None，当前节点为最近公共祖先
-            return root
-        if left: # 只有左边不为None，继续在左边搜索
-            return left 
-        return right # 否则继续在右边搜索
+        # brutal: 记录root走到p的路径，再记录root走到q的路径，对比，最后一个相同的节点即是答案
+        # 转化为 当前节点root是不是lowestCommonAncestor的问题：站在当前节点，去左右子树里找p or q, 如果左子树/右子树各有一个p/q, 那么当前节点就是答案； p/q都在一侧，那就走到root.left(or root.right)再看 -> 递归
+        
+        def dfs(node: 'TreeNode'):
+            if node is p or node is q or node is None:
+                return node # 找到p，找到q，没找到
+            
+            left = dfs(node.left) # p or q or None
+            right = dfs(node.right) # p or q or None
 
+            if left and right: # 都不是None
+                return node # 找到答案
+
+            return left or right # 递归
+        
+        return dfs(root)
         # O(n);O(n)
+
+            
