52140833ed6e5e7a441f052281aa67699c6f7e14
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-07-21 00:10:44 +0800

    Time: 20 ms (18.9%), Space: 19.2 MB (92.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,15 +1,20 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # hash+双指针
-        seen = defaultdict(int)
-        longest = 0
-        left = 0
+        # brutal: for i in range(n) 里面套 for j in range(i,n), 枚举所有子串，筛选是否有重复字符，记录长度
+        # 去重: hash
+        # 避免嵌套循环: 使用双指针
+        n = len(s)
+        cur_max = 0
+        left = right = 0
+        char_freq = defaultdict(int) # 哈希表 char: freq
 
-        for right,c in enumerate(s):
-            seen[c] += 1
-            while seen[c] > 1:
-                seen[s[left]] -= 1
+        for right in range(n):
+            char_freq[s[right]] += 1
+            while char_freq[s[right]] > 1:
+                # 删除记录，缩小窗口
+                char_freq[s[left]] -= 1
                 left += 1
-            longest = max(longest, right-left+1)
-        return longest
-        # O(n)每个c最多被左右指针各访问一次;O(256)
+            # 记录长度
+            cur_max = max(cur_max,right-left+1)
+        return cur_max
+        # O(n) 每个char被访问一次；O(26)
