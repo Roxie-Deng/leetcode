f0f71dee8648b9503a9dfbbd63238769226e0ed8
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-07-20 17:31:00 +0800

    Time: 2 ms (4.51%), Space: 19.5 MB (24.97%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -5,26 +5,40 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        dummy = ListNode(0,head)
-        prev = dummy
-
-        # move prev to the node just before the left
+        # 1. 找到要reverse的头尾节点， 断链
+        dummy = ListNode(next = head)
+        start = end = dummy
         for _ in range(left-1):
-            prev = prev.next # original left-1
-        
-        cur = prev.next # the start of sublist, i.e. left node
-        tail = cur # original left
+            start = start.next # 例1中找到1
+        for _ in range(right):
+            end = end.next # 4
 
-        # reverse the sublist
-        pre = None
-        for _ in range(right-left+1): # the numer of nodes: reset every node's next
-            temp = cur.next
-            cur.next = pre
-            pre = cur
-            cur = temp
-        # reconnect the sublist to the original list
-        # left-1 → right...left → right+1
-        prev.next = pre
-        tail.next = cur
+        very_start = start # 备份1
+        start = start.next # 2
+        very_start.next = None # 1,2 断链
+        
+        very_end = end.next # 备份5
+        end.next = None # 断链4，5
+
+        # 2. reverse
+        def reverse(head: Optional[ListNode])-> Optional[ListNode]:
+            pre = None
+            cur = head
+            while cur:
+                # 备份nxt
+                nxt = cur.next
+                # reverse
+                cur.next = pre
+                # move
+                pre = cur
+                cur = nxt
+            return pre
+
+        new_between = reverse(start) # 从2出发反转；返回4->3->2
+
+        # 3. 链接
+        very_start.next = new_between # 1->4->3->2
+        start.next = very_end # 2->5
 
         return dummy.next
+        # O(n); O(1)
