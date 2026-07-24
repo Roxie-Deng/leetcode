dc41f891f93bbf072489f2492490fcb161a20a3c
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-07-24 21:21:10 +0800

    Time: 68 ms (71.21%), Space: 30.9 MB (66.09%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,15 +1,15 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # 构建size为k的小根堆（根最小）装nums中最大的k个数，那么根就是nums中第k大的数
+        # brutal:排序
+        # heap: 小根堆，维护一个size为k的heap，遍历完arr后根就是arr中第k大的数
         min_heap = []
 
         for num in nums:
-            if len(min_heap)<k:
-                heapq.heappush(min_heap,num) # 堆未满直接加入,堆维持min_heap[0]最小 # heapq模块,(堆列表,item)参数原地修改
+            if len(min_heap)<k: # heap没满，num直接加入
+                heapq.heappush(min_heap,num)
             else:
-                if num>min_heap[0]:
-                    heapq.heapreplace(min_heap,num) #等价于pop(heap)+push(heap,item)
-        
+                # 当有比小根堆root更大的数字时，num加入heap
+                if min_heap[0]<num:
+                    heapq.heapreplace(min_heap,num) # heapq复杂度logk
         return min_heap[0]
-        # 遍历n,二叉树操作log k -> O(nlogk)
-        # O(k)
+        # O(nlogk);O(k)
