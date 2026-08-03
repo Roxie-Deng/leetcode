87a2738c1a35237901237173a22e3e7548b30808
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-08-03 14:11:53 +0800

    Time: 3 ms (61.21%), Space: 20.4 MB (52%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -4,6 +4,8 @@ class Solution:
         # f(i) = f(i-1)， if prices[i] <= prices[i-1]
         # f(i) = f(i-1) + prices[i]-prices[i-1]
         # DP 问题
+
+        '''
         n = len(prices)
         dp = [0] *(n+1)
 
@@ -16,4 +18,17 @@ class Solution:
 
         return dp[n]
         # O(n);O(n)
-        
+        '''
+
+        # 继续优化：可以不用数组记录状态，而用一个变量
+        cur_max = 0
+        n = len(prices)
+
+        for i in range(1,n):
+            cur_pro = prices[i] - prices[i-1]
+            # if cur_pro <= 0: cur_max维持原状
+            if cur_pro > 0:
+                cur_max += cur_pro
+        return cur_max
+        # O(n);O(1)
+            
