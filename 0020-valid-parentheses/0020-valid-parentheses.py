783d9b40fa57e700011aea8cc69af1ae287b1acf
Yuqi Deng <144052992+Roxie-Deng@users.noreply.github.com>
2026-09-18 21:43:33 +0800

    Time: 3 ms (32.61%), Space: 19.4 MB (5.36%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,40 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # 配对处理
-        '''
-        def isPair(c1:str,c2:str) -> bool:
-            pair = c1+c2
-            if pair == "()" or pair == "[]" or pair == "{}":
-                return True
-            return False
+        search_dict = {")":"(","]":"[","}":"{"}
+        opening = []
 
-        if len(s) == 0 or len(s) % 2 != 0:
-            return False
-
-        left = {'(','[','{'}
-        right = {')',']','}'}
-        # 利用stack后进先出的特性，遇到left就入栈，遇到right就left和right一起出栈,最后检查栈是否为空
-        ans = []
-
-        for i in range(len(s)):
-            if s[i] in left:
-                ans.append(s[i])
-            if s[i] in right:
-                if not ans or not isPair(ans[-1],s[i]):
+        for char in s:
+            if char in search_dict.values(): # opening
+                opening.append(char)
+            elif char in search_dict: # closing
+                if not opening or search_dict[char] != opening[-1]:
                     return False
-                ans.pop()
-        return ans == []
-        # O(n); O(n)
-        '''
-        # 继续优化：可以用映射表代替isPair辅助函数
-        mapping = {')': '(', ']': '[', '}': '{'}
-        stack = []
+                opening.pop()
+        
+        return not opening
 
-        for c in s:
-            if c not in mapping: # 左括号
-                stack.append(c)
-            else: # 右括号
-                if not stack or stack[-1] != mapping[c]: # 栈为空或者不匹配
-                    return False
-                stack.pop()
-        return stack == []
+        # O(n);O(n)
